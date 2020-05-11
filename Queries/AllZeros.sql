22d5d0d74881cab045626f2739b148d4411cde2f
Masson Christofferson <massonchristofferson@gmail.com>
2020-05-11 15:10:46 -0600

    cleaned up AllZeros a little

--- Queries/AllZeros.sql
+++ Queries/AllZeros.sql
@@ -6,7 +6,7 @@ JOIN (
 	FROM Dump 
 	WHERE elevation <= 1.5 
 	GROUP BY bc, muzzle_velocity
-) x ON x.bc = y.bc AND x.muzzle_velocity = y.muzzle_velocity
+) x ON x.bc = y.bc AND x.muzzle_velocity = y.muzzle_velocity AND y.zero = x.zero
 WHERE y.elevation = max_el
 GROUP BY y.bc, y.muzzle_velocity, y.zero
 ORDER BY y.bc, y.muzzle_velocity, range DESC
